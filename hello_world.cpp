74fc02850cd34ec7a87113134e5bbc7f2f1a86a8
BuildTools <no-reply>
2017-09-16 23:50:07 -0400

    Clean up errors

--- hello_world.cpp
+++ hello_world.cpp
@@ -4,6 +4,7 @@ using namespace std;
 void main() {
 	int number;
 	cout << "Hello World !!" << endl;
-	cin >> "Enter a number: " >> number >> endl;
+	cout << "Enter a number: "; 
+	cin >> number;
 	cout << "You entered the number " << number << endl;
 }
